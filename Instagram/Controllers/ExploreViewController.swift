eb5aa81f61d6b9d639ecd80c4ee2cefb2ef20b9b
joel <joelcrawford30@gmail.com>
2021-07-06 19:56:19 +0300

    Created New Controllers for Profile, Notifications, Camera, Explore

---
+++ Instagram/Controllers/ExploreViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ExploreViewController.swift
+//  Instagram
+//
+//  Created by JOEL CRAWFORD on 7/6/21.
+//  Copyright © 2021 JOEL CRAWFORD. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+
+}
